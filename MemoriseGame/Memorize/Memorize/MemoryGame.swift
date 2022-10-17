5ddfe761f5aae2d8c1b16e87c8241ccd0f47fe85
Ayon Chowdhury <md.rajib.sarwar@gmail.com>
2022-10-17 16:28:26 -0400

    removed all logical dependencies from view to viewmodel

--- MemoriseGame/Memorize/Memorize/MemoryGame.swift
+++ MemoriseGame/Memorize/Memorize/MemoryGame.swift
@@ -15,8 +15,8 @@ struct MemoryGame<CardContent> {
         //add numberOfPairsOfCards x 2 cards to cards array
         for pairIndex in 0..<numberOfPairsOfCards {
             let content = createCardContent(pairIndex)
-            cards.append(Card(content: content))
-            cards.append(Card(content: content))
+            cards.append(Card(content: content, id: pairIndex*2))
+            cards.append(Card(content: content, id: pairIndex*2+1))
         }
     }
     
@@ -24,10 +24,11 @@ struct MemoryGame<CardContent> {
         
     }
     
-    struct Card {
+    struct Card: Identifiable {
         var isFaceUp: Bool = false
         var isMatched: Bool = false
         var content: CardContent
+        var id: Int
     }
 }
 
